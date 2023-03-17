abb86426b8d4d64532f8894b8462af07d0bd1bd8
vaibhavkadwey <vaibhav3kadwey@gmail.com>
2023-03-17 13:35:01 +0530

    Added alert dailog

--- src/alerts/alert.js
+++ src/alerts/alert.js
@@ -1,20 +1,33 @@
 import React from "react";
-import { FiAlertCircle } from "react-icons/fi";
+import { FiAlertTriangle, FiAlertCircle, FiInfo } from "react-icons/fi";
+import { IoMdCheckmarkCircleOutline } from "react-icons/io";
 
 import "./alert.scss";
 
-const Alert = () => {
+const Alert = ({ alertType, alertMessage }) => {
+  const alertTypeBg = {
+    error: "rgb(253, 237, 237)",
+    warning: "rgb(255, 244, 229)",
+    info: "rgb(229, 246, 253)",
+    success: "rgb(237, 247, 237)",
+  };
+
+  const alertIcon = {
+    error: <FiAlertTriangle size={18} color="#d32f2f" />,
+    warning: <FiAlertCircle size={18} color="#ed6c02" />,
+    info: <FiInfo size={18} color="#0288d1" />,
+    success: <IoMdCheckmarkCircleOutline size={18} color="#2e7d32" />,
+  };
   return (
-    <span className="alert__container">
+    <div
+      className="alert__container"
+      style={{ backgroundColor: alertTypeBg[alertType] }}
+    >
       <div className="alert__section">
-        <div className="alert__section--icon">
-          <FiAlertCircle size={18} />
-        </div>
-        <p className="alert__section--message">
-          This message is regarding some error
-        </p>
+        <div className="alert__section--icon">{alertIcon[alertType]}</div>
+        <p className="alert__section--message">{alertMessage}</p>
       </div>
-    </span>
+    </div>
   );
 };
 
--- src/alerts/alertSection.js
+++ src/alerts/alertSection.js
@@ -2,7 +2,27 @@ import React from "react";
 import Alert from "./alert";
 
 const AlertSection = () => {
-  return <Alert />;
+  return (
+    <div>
+      <h1 style={{ color: "white" }}>Alert Section</h1>
+      <Alert
+        alertType="error"
+        alertMessage=" This message is regarding some error"
+      />
+      <Alert
+        alertType="warning"
+        alertMessage=" This message is regarding some warning"
+      />
+      <Alert
+        alertType="info"
+        alertMessage=" This message is regarding some info"
+      />
+      <Alert
+        alertType="success"
+        alertMessage=" This message is regarding some success"
+      />
+    </div>
+  );
 };
 
 export default AlertSection;
